0a7f14ff132b363b8a1a9de5366b476e0f7ebbe9
Rich Hopkins <hopmedic@hotmail.com>
2023-09-19 21:25:59 -0400

    End of section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
+import { act } from 'react-dom/test-utils';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(response => {
+        setActivities(response.data)
+      })
+  }, []);
+
   return (
-    <h1>Reactivities</h1>
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'></Header>
+      <List>
+        {activities
+          .sort((a, b) => a.date > b.date? 1 : -1)
+          .map((activity: any) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
   )
 }
 
