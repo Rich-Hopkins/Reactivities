a0db6daa581fb3f642f813dac1bf59c5938aeebb
Rich Hopkins <hopmedic@hotmail.com>
2023-12-07 21:55:47 -0500

    Add NavBar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
+import NavBar from './NavBar';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -16,7 +17,7 @@ function App() {
 
   return (
     <div>
-      <Header as='h2' icon='users' content='Reactivities'></Header>
+      <NavBar />
       <List animated bulleted>
         {activities
           .sort((a, b) => a.date > b.date ? 1 : -1)
