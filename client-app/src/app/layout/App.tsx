f4a7c070efc669ce68beddbb633a88795c23d5ee
Rich Hopkins <hopmedic@hotmail.com>
2023-12-19 19:56:06 -0500

    Fixing messed up commits

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,29 @@
+import { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities')
+      .then(response => {
+        setActivities(response.data)
+      })
+  },
+    []);
+
+  return (
+    <Fragment>
+      <NavBar />
+      <Container style={{ marginTop: '5em' }}>
+        <ActivityDashboard activities={activities} />
+      </Container>
+    </Fragment>
+  )
+}
+
+export default App
