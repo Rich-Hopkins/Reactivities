9a0f769598af0bd9f6a9b5c8e45a7ec204dc7fd4
Rich Hopkins <hopmedic@hotmail.com>
2023-12-20 17:26:18 -0500

    Add CRUD ops, loading indicator

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,9 +6,10 @@ interface Props {
     closeForm: () => void;
     activity: Activity | undefined;
     createOrEdit: (activity: Activity) => void;
+    submitting: boolean;
 }
 
-export default function ActivityForm({ closeForm, activity: selectedActivity, createOrEdit }: Props) {
+export default function ActivityForm({ closeForm, activity: selectedActivity, createOrEdit, submitting }: Props) {
 
     const initialState: Activity = selectedActivity ?? {
         id: '',
@@ -44,7 +45,7 @@ export default function ActivityForm({ closeForm, activity: selectedActivity, cr
                 <Form.Input type="date" placeholder='Date' value={activity.date} name="date" onChange={handleInputChange} />
                 <Form.Input placeholder='City' value={activity.city} name="city" onChange={handleInputChange} />
                 <Form.Input placeholder='Venue' value={activity.venue} name="venue" onChange={handleInputChange} />
-                <Button onClick={handleFormSubmit} floated='right' positive type='submit' content='Submit' />
+                <Button loading={submitting} onClick={handleFormSubmit} floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel' />
             </Form>
         </Segment>
