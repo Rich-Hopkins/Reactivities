bfffad0b53baae24f86992d705e0eeef6319cd4c
Rich Hopkins <hopmedic@hotmail.com>
2023-12-21 22:08:54 -0500

    Style Home Page

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,22 +1,12 @@
-import { SyntheticEvent, useState } from "react";
 import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import { Link } from "react-router-dom";
-import { useStore } from "../../../app/stores/store";
 
 interface Props {
     activity: Activity;
 }
 
 export default function ActivityListItem({ activity }: Props) { 
-    const { activityStore } = useStore();
-    const { deleteActivity, loading } = activityStore;
-    const [target, setTarget] = useState('');
-
-    function handleActivityDelete(event: SyntheticEvent<HTMLButtonElement>, id: string) {
-        setTarget(event.currentTarget.name);
-        deleteActivity(id);
-    }
 
     return (
         <Segment.Group>
