484d7c80398ad0b577f398648481fcccba5fcbd0
Rich Hopkins <hopmedic@hotmail.com>
2023-12-27 18:23:10 -0500

    Add handling of unknown guid, End of section 10

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -10,71 +10,63 @@ namespace Persistence
 
             var activities = new List<Activity>
             {
-                new Activity
-                {
-                    Title = "Past Activity 1",
-                    Date = DateTime.UtcNow.AddMonths(-2),
-                    Description = "Activity 2 months ago",
-                    Category = "drinks",
-                    City = "London",
-                    Venue = "Pub",
+                new() {
+                    Title = "Christmas Train Rid",
+                    Date = new DateTime(2023, 12, 31),
+                    Description = "Taking a train ride in Chattanooga where you will ride through a six-mile, u-shaped tunnel, and see the Christmas lights.",
+                    Category = "railway",
+                    City = "Chattanooga",
+                    Venue = "Tennessee Valley Railroad Museum",
                 },
-                new Activity
-                {
-                    Title = "Past Activity 2",
-                    Date = DateTime.UtcNow.AddMonths(-1),
-                    Description = "Activity 1 month ago",
-                    Category = "culture",
-                    City = "Paris",
-                    Venue = "Louvre",
+                new() {
+                    Title = "Sun n Fun",
+                    Date = new DateTime(2024, 4, 9),
+                    Description = "Sun 'n Fun Aviation Expo is an annual air show held in Lakeland, Florida during the month of April. The event usually lasts for one week, and primarily features general aviation airshows and showplanes, but also includes other aviation-related activities, such as hot air ballooning, fly-ins, and educational forums.",
+                    Category = "flight",
+                    City = "Lakeland, FL",
+                    Venue = "KLAL",
                 },
-                new Activity
-                {
-                    Title = "Future Activity 1",
-                    Date = DateTime.UtcNow.AddMonths(1),
-                    Description = "Activity 1 month in future",
-                    Category = "culture",
-                    City = "London",
-                    Venue = "Natural History Museum",
+                new() {
+                    Title = "Aero Showcase",
+                    Date = new DateTime(2024, 10, 18),
+                    Description = "The Aero Showcase at DeLand, FL 2023 is a highly anticipated annual event that highlights the latest developments and advancements in Sport, Recreational, and General Aviation.",
+                    Category = "flight",
+                    City = "DeLand, FL",
+                    Venue = "KDED",
                 },
-                new Activity
-                {
-                    Title = "Future Activity 2",
+                new() {
+                    Title = "Beach Vacation",
                     Date = DateTime.UtcNow.AddMonths(2),
-                    Description = "Activity 2 months in future",
-                    Category = "music",
-                    City = "London",
-                    Venue = "O2 Arena",
+                    Description = "Time to spend a weekend on the beach in Gulf Shores, Alabama. The sands are white and the water is warm.",
+                    Category = "travel",
+                    City = "Gulf Shores, AL",
+                    Venue = "Beach",
                 },
-                new Activity
-                {
-                    Title = "Future Activity 3",
+                new() {
+                    Title = "Date Night",
                     Date = DateTime.UtcNow.AddMonths(3),
-                    Description = "Activity 3 months in future",
-                    Category = "drinks",
-                    City = "London",
-                    Venue = "Another pub",
+                    Description = "Time to go watch a movie.",
+                    Category = "film",
+                    City = "Gainesville, GA",
+                    Venue = "Movie Theater",
                 },
-                new Activity
-                {
-                    Title = "Future Activity 4",
+                new() {
+                    Title = "Dinner Date",
                     Date = DateTime.UtcNow.AddMonths(4),
-                    Description = "Activity 4 months in future",
-                    Category = "drinks",
-                    City = "London",
-                    Venue = "Yet another pub",
+                    Description = "Time for a nice dinner at our favorite seafood restaurant.",
+                    Category = "food",
+                    City = "Buford, GA",
+                    Venue = "Juicy Crab",
                 },
-                new Activity
-                {
-                    Title = "Future Activity 5",
+                new() {
+                    Title = "Museum Visit",
                     Date = DateTime.UtcNow.AddMonths(5),
-                    Description = "Activity 5 months in future",
-                    Category = "drinks",
-                    City = "London",
-                    Venue = "Just another pub",
+                    Description = "We will visit the High Museum of Art in Atlanta, GA.",
+                    Category = "culture",
+                    City = "Atlanta, GA",
+                    Venue = "High Museum of Art",
                 },
-                new Activity
-                {
+                new() {
                     Title = "Future Activity 6",
                     Date = DateTime.UtcNow.AddMonths(6),
                     Description = "Activity 6 months in future",
@@ -82,8 +74,7 @@ namespace Persistence
                     City = "London",
                     Venue = "Roundhouse Camden",
                 },
-                new Activity
-                {
+                new() {
                     Title = "Future Activity 7",
                     Date = DateTime.UtcNow.AddMonths(7),
                     Description = "Activity 7 months in future",
@@ -91,8 +82,7 @@ namespace Persistence
                     City = "London",
                     Venue = "Somewhere on the Thames",
                 },
-                new Activity
-                {
+                new() {
                     Title = "Future Activity 8",
                     Date = DateTime.UtcNow.AddMonths(8),
                     Description = "Activity 8 months in future",
