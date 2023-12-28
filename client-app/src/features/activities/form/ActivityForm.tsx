db385b4b67dc2e620c9f1396f6d5bf6e068dd16f
Rich Hopkins <hopmedic@hotmail.com>
2023-12-27 19:10:42 -0500

    Add validation with Yup

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
-import { Button, Segment } from "semantic-ui-react";
+import { Button, FormField, Label, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { v4 as uuid } from 'uuid';
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from 'yup';
 
 
 export default observer(function ActivityForm() {
@@ -30,6 +31,15 @@ export default observer(function ActivityForm() {
         venue: ''
     });
 
+    const validationSchema = Yup.object({
+        title: Yup.string().required('The activity title is required'),
+        description: Yup.string().required('The activity description is required'),
+        category: Yup.string().required(),
+        date: Yup.string().required('Date is required').nullable(),
+        venue: Yup.string().required(),
+        city: Yup.string().required()
+    });
+
     useEffect(() => {
         if (id) loadActivity(id).then(activity => setActivity(activity!));
     }, [id, loadActivity]);
@@ -52,10 +62,19 @@ export default observer(function ActivityForm() {
 
     return (
         <Segment clearing>
-            <Formik enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
+            <Formik
+                enableReinitialize
+                validationSchema={validationSchema}
+                initialValues={activity}
+                onSubmit={values => console.log(values)}>
                 {({ handleSubmit }) => (
                     <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
-                        <Field placeholder='Title' name="title" />
+                        <FormField>
+                            <Field placeholder='Title' name="title" />
+                            <ErrorMessage
+                                name='title'
+                                render={error => <Label basic color='red' content={error} />} />
+                        </FormField>
                         <Field placeholder='Description' name="description" />
                         <Field placeholder='Category' name="category" />
                         <Field type="date" placeholder='Date' name="date" />
@@ -64,7 +83,8 @@ export default observer(function ActivityForm() {
                         <Button
                             loading={loading}
                             floated='right'
-                            positive type='submit'
+                            positive
+                            type='submit'
                             content='Submit' />
                         <Button
                             as={Link} to='/activities'
