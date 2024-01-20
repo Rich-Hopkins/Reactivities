326c93726050b186c92b920dbbdaa9b11f2c2dae
Rich Hopkins <hopmedic@hotmail.com>
2024-01-19 23:25:25 -0500

    persisting login

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,27 +1,41 @@
-import { Fragment} from 'react';
-import { Container } from 'semantic-ui-react';
-import NavBar from './NavBar';
-import { observer } from 'mobx-react-lite';
-import { Outlet, useLocation } from 'react-router-dom';
-import HomePage from '../../features/home/HomePage';
-import { ToastContainer } from 'react-toastify';
+import { Fragment, useEffect } from "react";
+import { Container } from "semantic-ui-react";
+import NavBar from "./NavBar";
+import { observer } from "mobx-react-lite";
+import { Outlet, useLocation } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import { ToastContainer } from "react-toastify";
+import { useStore } from "../stores/store";
 
 function App() {
   const location = useLocation();
+  const { userStore, commonStore } = useStore();
+
+  useEffect(() => {
+    if (commonStore.token) {
+      userStore.getUser().finally(() => commonStore.setAppLoaded());
+    } else {
+      commonStore.setAppLoaded();
+    }
+  }, [commonStore, userStore]);
+
+  if (!commonStore.appLoaded) return (<h1>Loading...</h1>)
 
   return (
     <Fragment>
-      <ToastContainer position='bottom-right' theme='colored' />
-      {location.pathname === '/' ? <HomePage /> : (
+      <ToastContainer position="bottom-right" theme="colored" />
+      {location.pathname === "/" ? (
+        <HomePage />
+      ) : (
         <>
           <NavBar />
-          <Container style={{ marginTop: '5em' }}>
+          <Container style={{ marginTop: "5em" }}>
             <Outlet />
           </Container>
         </>
       )}
     </Fragment>
-  )
+  );
 }
 
 export default observer(App);
--- client-app/src/app/stores/UserStore.ts
+++ client-app/src/app/stores/UserStore.ts
@@ -4,28 +4,36 @@ import agent from "../api/agent";
 import { store } from "./store";
 import { router } from "../router/Routes";
 
-export default class UserStore{
+export default class UserStore {
   user: User | null = null;
 
-  constructor(){
+  constructor() {
     makeAutoObservable(this);
   }
 
-  get isLoggedIn(){
+  get isLoggedIn() {
     return !!this.user;
   }
 
   login = async (creds: UserFormValues) => {
-      const user = await agent.Account.login(creds);
-      store.commonStore.setToken(user.token);
-      runInAction(() => this.user = user);
-      router.navigate('/activities');
-  }
+    const user = await agent.Account.login(creds);
+    store.commonStore.setToken(user.token);
+    runInAction(() => (this.user = user));
+    router.navigate("/activities");
+  };
 
-  logout=() => {
+  logout = () => {
     store.commonStore.setToken(null);
-    localStorage.removeItem('jwt');
     this.user = null;
-    router.navigate('/');
-  }
+    router.navigate("/");
+  };
+
+  getUser = async () => {
+    try {
+      const user = await agent.Account.current();
+      runInAction(() => (this.user = user));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
