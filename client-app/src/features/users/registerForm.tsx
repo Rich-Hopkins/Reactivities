8aad56ffbdec0d1f0f2962905b062c29bc4ab641
Rich Hopkins <hopmedic@hotmail.com>
2024-01-24 22:10:00 -0500

    End of section 13 - Errors in api and displayed in UI

--- client-app/src/features/users/registerForm.tsx
+++ client-app/src/features/users/registerForm.tsx
@@ -2,8 +2,9 @@ import { ErrorMessage, Form, Formik } from "formik";
 import MyTextInput from "../../app/common/form/MyTextInput";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../../app/stores/store";
-import { Button, Header, Label } from "semantic-ui-react";
+import { Button, Header } from "semantic-ui-react";
 import * as Yup from 'yup';
+import ValidationErrors from "../errors/ValidationErrors";
 
 export default observer(function RegisterForm() {
   const { userStore } = useStore();
@@ -14,7 +15,7 @@ export default observer(function RegisterForm() {
       onSubmit={(values, { setErrors }) =>
         userStore
           .register(values)
-          .catch(() => setErrors({ error: "Invalid email or password" }))}
+          .catch((error) => setErrors({ error }))}
       validationSchema={Yup.object({
         displayName: Yup.string().required(),
         username: Yup.string().required(),
@@ -24,7 +25,7 @@ export default observer(function RegisterForm() {
     >
 
       {({ handleSubmit, isSubmitting, errors, isValid, dirty }) => (
-        <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+        <Form className="ui form error" onSubmit={handleSubmit} autoComplete="off">
           <Header as='h2' content='Register for Reactivities' color='teal' textAlign='center' />
           <MyTextInput name="displayName" placeholder="Display Name" />
           <MyTextInput name="username" placeholder="Username" />
@@ -33,12 +34,7 @@ export default observer(function RegisterForm() {
           <ErrorMessage
             name="error"
             render={() => (
-              <Label
-                style={{ marginBottom: 10 }}
-                basic
-                color="red"
-                content={errors.error}
-              />
+              <ValidationErrors errors={errors.error as unknown as string[] }/>
             )}
           />
           <Button
